fe0e716ffa99ea1977d27033c2f91ef9eb4353b7
wanghong <htimeis007money>
2020-03-23 16:59:40 +0800

    init

---
+++ src/main/java/com/hong/limit/Application.java
@@ -0,0 +1,17 @@
+package com.hong.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author wanghong
+ * @Date 2020/3/23 16:56
+ * @Version V1.0
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
